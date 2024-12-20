b78a29b51dd152d614cf19cae9495b7fdf58d0f8
Mahadev949 <167230806+Mahadev949@users.noreply.github.com>
2024-12-20 11:54:42 +0530

    Create print 1 to n.c

---
+++ print 1 to n.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("enter the nth value :- ");
+    scanf("%d",&n);
+    for (int i=1;i<=n;i++){
+        printf("%d ",i);
+    }
+    
+    return 0;
+}
