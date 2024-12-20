926ca07bcde51fbc14a1fcd1b9d0dcf72cdb58cf
Mahadev949 <167230806+Mahadev949@users.noreply.github.com>
2024-12-20 12:07:39 +0530

    Create sum of numebrs.c

---
+++ sum of numebrs.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a,b,c=0;
+    printf("Enter the number :- ");
+    scanf("%d",&a);
+    while (a>0){
+        b=a%10;
+        c+=b;
+        a=a/10;
+        
+    }
+    printf("%d",c);
+    return 0;
+}
